dd6ba1d5602d5ea2058e8d0ba09eb9484147e1b7
adityaDalai1 <aditya.dalaiii312@gmail.com>
2024-12-18 12:39:10 +0000

    cluster connected

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-
 const express = require('express');
-const connectDB = require('./config/db');
+const cors = require('cors');
+const dotenv = require("dotenv")
+// const path = require('path');
+
+const connectDB = require('./config/db'); // Import the database connection function
 const roomRoutes = require('./routes/roomRoutes'); // Import room routes
+dotenv.config({ path: "./config.env"})
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
 
-// Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON requests
+app.use(cors());
 
-// Basic route for home page
 app.get("/", (req, res) => {
-    res.send("Home Page of the Rental Management App");
+    res.send("HomePage Of The Rental Management App"); // Home page route
 });
 
-// Use room routes with prefix '/api'
-app.use('/api', roomRoutes);
+// // SERVE STATIC FILES
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (_, res) {
+//     res.sendFile(
+//         path.join(__dirname, "./client/build/index.html"),
+//         function (err) {
+//             res.status(500).send(err);
+//         }
+//     );
+// });
 
+app.use('/api', roomRoutes); // Use room routes with prefix '/api'
+
+const PORT = process.env.PORT || 5000;
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
